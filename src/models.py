324b77c1ccf205f232e85bba47e8359faa7ec170
Alejandro Novellino <alnovellino07@gmail.com>
2021-06-08 17:30:57 -0400

    Models started

--- src/models.py
+++ src/models.py
@@ -1,12 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    salt = db.Column(db.String(120))
+    hashed_password = db.Column(db.String(80), unique=False, nullable=False)
+    #is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -17,3 +20,12 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Admin(db.Model, User):
+    __tablename__ = 'admin'
+    id = db.Column(db.Integer, primary_key=True)
+
+
+class Professor(db.Model, User):
+    __tablename__ = 'professor'
+    id= db.Column(db.Integer, primary_key=True)
